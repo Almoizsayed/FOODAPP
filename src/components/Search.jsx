413e0aba1250ebf7a60f7f0887409a24b3338d50
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-06 19:56:41 +0530

    create state to save food

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "313106a8867f484bae38e6f4fe77cfeb";
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
 
   useEffect(() => {
@@ -9,6 +9,7 @@ export default function Search() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
