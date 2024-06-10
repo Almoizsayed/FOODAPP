2fa891717b69ee7c706d11e5b0bafbb0a63e9252
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-10 19:49:23 +0530

    Fetching Ingredients

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -45,7 +45,23 @@ export default function FoodDetails({ foodId }) {
             <strong>{food.pricePerServing / 100} per Serving </strong>
           </span>
         </div>
-
+        <h2>Ingredients</h2>
+        {food.extendedIngredients.map((item) => (
+          <div>
+            {" "}
+            <img
+              src={
+                `https://spoonacular.com/cdn/ingredients_100x100/` + item.image
+              }
+              alt=""
+            />
+            <h3> {item.name}</h3>
+            <h3>
+              {" "}
+              {item.amount} {item.unit}
+            </h3>
+          </div>
+        ))}
         <h2> Instructions </h2>
         <div className={styles.recipeInstructions}>
           <ol>
