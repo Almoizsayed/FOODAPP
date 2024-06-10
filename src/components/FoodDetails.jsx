a6eece6d20044d3ed518371f0bdae3f505a72083
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-10 15:27:22 +0530

    Fetching Reciepi from an API

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function FoodDetails({ foodId }) {
-  return <div> Food Details {foodId} </div>;
+  //   console.log(`foodId from props: ${foodId}`);
+  const [food, setFood] = useState({});
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "c10160ce1ada4012b02a69ada55e0089";
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+    }
+    fetchFood();
+  }, [foodId]);
+  return (
+    <div>
+      {" "}
+      Food Details {foodId} {food.title}
+      <img src={food.image} alt="" />
+    </div>
+  );
 }
