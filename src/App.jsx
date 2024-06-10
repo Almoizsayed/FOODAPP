2abfe466aa61638e9737f0ed79f54d45e2dccaaa
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-10 19:29:28 +0530

    Designing the Recipe Detail Component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import InnerContainer from "./components/InnerContainer";
 import FoodDetails from "./components/FoodDetails";
 function App() {
   const [foodData, setFoodData] = useState([]);
-  const [foodId, setFoodId] = useState("680975");
+  const [foodId, setFoodId] = useState("658920");
   return (
     <div className="App">
       <Nav />
--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import styles from "./fooddetails.module.css";
 export default function FoodDetails({ foodId }) {
   //   console.log(`foodId from props: ${foodId}`);
   const [food, setFood] = useState({});
@@ -18,10 +18,10 @@ export default function FoodDetails({ foodId }) {
   }, [foodId]);
   return (
     <div>
-      <div>
-        <h1> {food.title} </h1>
-        <img src={food.image} alt="" />
-        <div>
+      <div className={styles.recipeCard}>
+        <h1 className={styles.recipeName}> {food.title} </h1>
+        <img className={styles.recipeImage} src={food.image} alt="" />
+        <div className={styles.recipeDetails}>
           <span>
             {" "}
             <strong> {food.readyInMinutes} Minutes </strong>
@@ -30,21 +30,34 @@ export default function FoodDetails({ foodId }) {
             {" "}
             <strong>Serves {food.servings} </strong>
           </span>
-          <span>{food.vegetarian ? "vegetarian" : "Non-Vegetarian"}</span>
-          <span>{food.vegan ? "Vegan" : ""}</span>
+          <span>
+            <strong>{food.vegetarian ? "vegetarian" : "Non-Vegetarian"}</strong>
+          </span>
+          <span>
+            <strong>{food.vegan ? "Vegan" : ""}</strong>
+          </span>
+          {/* <span>{food.pricePerServing / 100} per Serving </span> */}
         </div>
         <div>
           {" "}
-          $<span>{food.pricePerServing / 100} per Serving </span>
+          $
+          <span>
+            <strong>{food.pricePerServing / 100} per Serving </strong>
+          </span>
         </div>
-      </div>
-      <div>
+
         <h2> Instructions </h2>
-        {IsLoading ? (
-          <p>Loading...</p>
-        ) : (
-          food.analyzedInstructions[0].steps.map((step) => <li>{step.step}</li>)
-        )}
+        <div className={styles.recipeInstructions}>
+          <ol>
+            {IsLoading ? (
+              <p>Loading...</p>
+            ) : (
+              food.analyzedInstructions[0].steps.map((step) => (
+                <li>{step.step}</li>
+              ))
+            )}
+          </ol>
+        </div>
       </div>
     </div>
   );
